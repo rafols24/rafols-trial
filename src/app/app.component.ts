d0f8bd16abceb0c3073d2414f1c20023b2760b94
rafols24 <emelisarafols@gmail.com>
2021-07-22 08:27:07 +0800

    Activity 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,46 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, OnInit ,Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <p>My First Angular Component Count</p>
+  <br>
+  <p>Count:{{count}}</p>
+  <br>
+  <button (click)="AddEvent()">AddCount</button>
+  <br>
+  <button (click)="SubEvent()">SubCount</button>
+  <h5 *ngIf="count > 5" >second works!</h5>
+  `
+       
+  ,
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Trial Activity';
-  appMessage = "";
+export class AppComponent implements OnInit {
+  
+  @Output() appEvent = new EventEmitter();
+  constructor() { }
 
-  appEvent(event:any){
-    this.appMessage = event;
+  ngOnInit(): void {
   }
+
+  AddEvent() {
+    this.appEvent.emit(this.count++);
+  
+  }
+  SubEvent(){
+    this.appEvent.emit(this.count--);
+  }
+
+  // title = 'Trial Activity';
+  // appMessage = "";
+  count = 0;
+
+  // appEvent(event:any){
+  //   this.appMessage = event;
+  // }
+
+  
 
 }
